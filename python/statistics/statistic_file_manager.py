2165e92cf8a5555313809345420a3ba0c7eae8ca
hoxisword <hoxisword@gmail.com>
2024-05-11 12:34:53 +0200

    Python documentation

--- python/statistics/statistic_file_manager.py
+++ python/statistics/statistic_file_manager.py
@@ -2,6 +2,10 @@ import os
 
 import numpy as np
 
+"""
+Cette classe permet de lire un fichier généré par l'application statistiques et de le transformer en objet python.
+On peut par la suite afficher correctement les données du fichier.
+"""
 class StatisticFile:
     def __init__(self, file_name):
         self.file_name = file_name
@@ -38,22 +42,40 @@ class StatisticFile:
             self.total_simu = total_simu
         self.data_raw = np_data_raw
 
+    """
+    Retourne les données brutes du fichier
+    """
     def get_data(self):
         return self.data_raw
 
+    """
+    Retourne les coefficients (affine) permettant de retrouver le domaine d'échantillonnage de l'étude
+    """
     def get_coefficients(self):
         return self.coefficients
 
+    """
+    Retourne les dimensions des données
+    """
     def get_shape(self):
         return self.shape
 
+    """
+    Retourne la matrice des nombres totaux de simulations suivant les valeurs de l'étude
+    """
     def get_total_simu(self):
         return self.total_simu
 
+    """
+    Retourne le nom de la simulation
+    """
     def get_name(self):
         return self.name
 
 
+"""
+Permet de lire tous les fichiers de statistiques d'un répertoire
+"""
 def read_statistic_files(directory):
     result = []
     for file_name in os.listdir(directory):
